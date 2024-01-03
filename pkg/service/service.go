966d53f5a830513abb9647236771ee1765730a82
Matteo Ponza <matteoponza@gmail.com>
2024-01-03 15:18:22 +0100

    wip: Add todo comment to Service interface

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -6,5 +6,6 @@ type Response struct {
 }
 
 type Service interface {
+	// TODO: we should change the response type to (Response, error)
 	ExecuteRequest(method string, path string, body []byte) Response
 }
