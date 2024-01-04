3e3d9b6ea401f950f751c2e69b07bd79c8cf0b54
Crystal Kwok <kwokyatingcrystal@gmail.com>
2024-01-04 11:37:57 +0100

    Add tests to test for complex JSON response

--- pkg/mapper/mapper_test.go
+++ pkg/mapper/mapper_test.go
@@ -141,6 +141,52 @@ func TestMapper(t *testing.T) {
 		assert.NoError(t, err)
 		assert.JSONEq(t, expected, actual)
 	})
+
+	t.SkipNow()
+
+	//to work on
+	t.Run("Read complex JSON response from file", func(t *testing.T) {
+
+		m, err := NewMapperFromFile("../router/config.json", "v2")
+		assert.NoError(t, err)
+
+		inputJson, err := readResponseFromFile("complex-response.json")
+		assert.NoError(t, err)
+
+		actual, err := m.Transform(inputJson)
+		var expected = `
+		{
+			"id": "1c68267f-0182-53e5-a3bd-3940b1f0c47e",
+			"start_date": "2022-05-10 00:00:00.000+0000",
+			"end_date": "2022-05-11 00:00:00.000+0000"			
+		}
+		`
+
+		assert.NoError(t, err)
+		assert.JSONEq(t, expected, actual)
+	})
+	
+	//to work on
+	t.Run("Read input JSON response from mobility-events file", func(t *testing.T) {
+
+		m, err := NewMapperFromFile("../router/config.json", "v2")
+		assert.NoError(t, err)
+
+		inputJson, err := readResponseFromFile("../response-samples/mobility-events.json")
+		assert.NoError(t, err)
+
+		actual, err := m.Transform(inputJson)
+		var expected = `
+		{
+			"id": "1c68267f-0182-53e5-a3bd-3940b1f0c47e",
+			"start_date": "2022-05-10 00:00:00.000+0000",
+			"end_date": "2022-05-11 00:00:00.000+0000"			
+		}
+		`
+
+		assert.NoError(t, err)
+		assert.JSONEq(t, expected, actual)
+	})
 }
 
 func readResponseFromFile(fileName string) (string, error) {
